460d8e6128d15d7bd17652367f4f7ff2351c63d4
Gamin03 <majm03@yeah.net>
2018-09-26 23:51:29 +0800

    Make minor revisions. 1. Add Excel and PNG files for result output. 2. Add readme for user manual. 3. Add dpa calculation methods and dpa results.

--- G-pka.py
+++ G-pka.py
@@ -21,6 +21,7 @@ Main for G-pka.
                    4) Zr-93,95,97 共振区域结果不正确！
                       => 已解决。注意(n,gamma)散射矩阵的入射和出射能群下标，以及计算特定群的上下群边界值，这些容易出错。
         2018/09/19 1) 增加dpa损伤计算功能
+                      dpa计算结果不对！PKA归一化结果对，但是PKAs 结果与SPECTER-PKA差1.32倍？
                    2) 输出结果格式的选择，输出最终PKA和dpa结果数据
                    3) 输出绘图格式，绘图的多种形式，绘图到文件
 
@@ -35,6 +36,7 @@ from read_pka_file import *
 from utility_pka import *
 from utility_fig import *
 from utility_basic import *
+from utility_output import *
 
 # Code version control
 _major_version = 0
@@ -43,13 +45,12 @@ _bugfix_version = 0
 print_code_title_version(_major_version, _minor_version, _bugfix_version)
 
 # Global variables
+atoms_per_mole = 0.6022e+24
+
 global_recoil = {}
 global_element = {}
 
-
-
 # --- Initialization ----------------------------------------------------------
-
 if len(sys.argv) == 1:
     input_file = 'input.json'
     output_file = 'output.txt'
@@ -63,7 +64,8 @@ else:
     raise ValueError('Input parameters ERROR!')
 
 inp = Input(input_file)
-output = open(output_file, 'w')
+# output = open(output_file, 'w')
+output = sys.stdout
 nuclides = []
 
 # Print title into output
@@ -72,7 +74,6 @@ print_code_title_version(_major_version, _minor_version, _bugfix_version, file=o
 energy_group = None  # =====Temp
 
 # --- Read the input file -----------------------------------------------------
-
 inp.read_infile(output)
 inp.read_flux(output)
 
@@ -136,17 +137,27 @@ for nuc in nuclides:
                                 nuc.recoil_energy_group_struc)
     recoil_pka_spectrum_total = np.zeros(nuc.num_recoil_energy_group_struc)
 
-    # Deal each nuclide
+    # Deal each nuclide 处理每个原始靶核
     for k, recoil_nuc in enumerate(nuc.recoil_nuclides_particles_info):
 
-        # Deal each reaction channel
+        # Deal each reaction channel 处理每个反应道
         # --- Set up the NuclideRecoil for this nuc
         za_recoil = get_daughter_nuclides_particles(recoil_nuc[0], recoil_nuc[1], nuc.Z, nuc.A)
         nuc_recoil = NuclideRecoil(za_recoil[0], za_recoil[1], recoil_nuc[1])
+        nuc_recoil.title = recoil_nuc[0]
         # --- Load the recoil matrix
         nuc_recoil.load_recoil_matrix(recoil_nuc[2])
         # --- Calculate recoil pka spectra and save it
         nuc_recoil.compute_recoil_pka_spectra(nuc.recoil_flux_pka)
+
+        # Calculate dpa values
+        if inp.do_damage:
+            nuc_recoil.damage_function_coeffs, nuc_recoil.estimate_ed = \
+                get_damage_coeffs_array(nuc.recoil_energy_group_struc, (nuc_recoil.Z, nuc_recoil.A), (nuc.Z, nuc.A))
+            nuc_recoil.damage_cross_section = nuc_recoil.damage_function_coeffs * nuc_recoil.pka_spectrum
+
+            nuc_recoil.damage_dpa = nuc_recoil.damage_cross_section * 0.8 / (2. * nuc_recoil.estimate_ed)
+
         nuc.append_recoil_nuclide(nuc_recoil)
 
         # Accumulate the NuclideRecoil for this nuc
@@ -155,93 +166,100 @@ for nuc in nuclides:
     # Save the total recoil pka spectrum for this nuc
     nuc.recoil_pka_spectrum = recoil_pka_spectrum_total
 
-# --- Output results ----------------------------------------------------------
+# --- Calculate total results ----------------------------------------------------------
 # Check each recoil nuclides of every initial nuclide, save them into global
 # nuclides and elements dictionary
+# 计算每个初始靶核的每个反冲核，并保存在全局的核素和元素字典内
 # ****** Add the nuclide ratio here ******
+# ****** 注意，在此处引入核素比值 ******
 print("\n\n>>> START CALCULATE TOTAL PKA RESULTS ...", file=output)
 for nuc in nuclides:
     ratio = nuc.ratio
-    for recoil_nuc in nuc.recoil_nuclides:
 
+    for recoil_nuc in nuc.recoil_nuclides:
+        # 对于每个反冲核，检查其核素或者元素在全局字典内是否存在
+        # 若存在，则将当前pka谱加上；若不存在，则创建核素或元素，保存数据，并添加到全局字典内
         # Check if the nuclide exist in the dict, if not, append it
         if recoil_nuc.name in global_recoil:
-            if ( (recoil_nuc.name != 'He-4') and (800 <= recoil_nuc.mtd <= 849) ) or \
-               ( (recoil_nuc.name != 'H-1')  and (600 <= recoil_nuc.mtd <= 649) ) :
-               continue
+            if ((recoil_nuc.name != 'He-4') and (800 <= recoil_nuc.mtd <= 849)) or \
+               ((recoil_nuc.name != 'H-1')  and (600 <= recoil_nuc.mtd <= 649)):
+                continue
             global_recoil[recoil_nuc.name].add_recoil_pka_spectrum(recoil_nuc.pka_spectrum * ratio)
+            if inp.do_damage:
+                global_recoil[recoil_nuc.name].add_damage_values(recoil_nuc, ratio)
         else:
             # Increase new nuclide into global nuclide dictionary
             nuclide = Nuclide((recoil_nuc.Z, recoil_nuc.A))
+            nuclide.recoil_energy_group_struc = nuc.recoil_energy_group_struc
             nuclide.recoil_pka_spectrum = recoil_nuc.pka_spectrum * ratio
+            if inp.do_damage:
+                nuclide.copy_recoil_damage(recoil_nuc, ratio)
             global_recoil[recoil_nuc.name] = nuclide
 
         # Check if the element exist in the dict, if not, append it
         if recoil_nuc.element in global_element:
-            if ( (recoil_nuc.name != 'He-4') and (800 <= recoil_nuc.mtd <= 849) ) or \
-               ( (recoil_nuc.name != 'H-1')  and (600 <= recoil_nuc.mtd <= 649) ) :
-               continue
+            if ((recoil_nuc.name != 'He-4') and (800 <= recoil_nuc.mtd <= 849)) or \
+               ((recoil_nuc.name != 'H-1')  and (600 <= recoil_nuc.mtd <= 649)):
+                continue
             global_element[recoil_nuc.element].add_recoil_pka_spectrum(recoil_nuc.pka_spectrum * ratio)
+            if inp.do_damage:
+                global_element[recoil_nuc.element].add_damage_values(recoil_nuc, ratio)
         else:
             # Increase new element into global element dictionary
             element = Element(recoil_nuc.Z)
             element.num_recoil_pka_energy_group = nuc.num_recoil_energy_group_struc
             element.copy_recoil_pka_energy_group(nuc.recoil_energy_group_struc)
             element.recoil_pka_spectrum = recoil_nuc.pka_spectrum * ratio
+            if inp.do_damage:
+                element.copy_recoil_damage(recoil_nuc, ratio)
             global_element[element.name] = element
+
+# Calculate average pka and dpa values
+# 计算平均的pka和dpa值
+for key in global_recoil:
+    nuclide = global_recoil[key]
+    # in eV
+    if sum(nuclide.recoil_pka_spectrum) > 0.:
+        nuclide.average_pka_energy = sum(nuclide.recoil_pka_spectrum * 0.5 * 1.E+6 *
+                        (nuclide.recoil_energy_group_struc[:-1] + nuclide.recoil_energy_group_struc[1:])) / \
+                        sum(nuclide.recoil_pka_spectrum)
+
+    nuclide.average_displacement_energy = sum(nuclide.damage_cross_section) * 1.E+6
+
+for key in global_element:
+    element = global_element[key]
+    # in eV
+    element.average_pka_energy = sum(element.recoil_pka_spectrum * 0.5 * 1.E+6 *
+                            (element.recoil_pka_energy_group[:-1] + element.recoil_pka_energy_group[1:])) / \
+                            sum(element.recoil_pka_spectrum)
+    element.average_displacement_energy = sum(element.damage_cross_section) * 1.E+6
 
 # ------ Sum results ------
 total_pka_spectrum = None
 for key in global_element:
     ele = global_element[key]
-    ele.calculate_average_pka_energy()
+    # ele.calculate_average_pka_energy()
     if total_pka_spectrum is None:
         total_pka_spectrum = ele.recoil_pka_spectrum
     else:
         total_pka_spectrum += ele.recoil_pka_spectrum
 
 
+# --- 输出结果 ----------------------------------------------------------------
+# 将结果写入Excel表
+if inp.do_write_each_nuclides:
+    for nuc in nuclides:
+        write_each_recoil_pka_into_xls(nuc)
+write_total_nuclides_into_xls(global_recoil)
+write_total_elements_into_xls(global_element)
+
 print("\n\n>>> OUTPUT TOTAL PKA RESULTS ...", file=output)
 
-element = global_element['Zr']
-print("\n#### {} ".format(element.name), file=output)
-for i in range(element.num_recoil_pka_energy_group):
-    print(" {0:12.4E} {1:12.4E} {2:12.4E}".format(element.recoil_pka_energy_group[i],
-                                                  element.recoil_pka_energy_group[i+1],
-                                                  element.recoil_pka_spectrum[i]), file=output)
-print("#    Average PKA energy = {}".format(element.average_pka_energy), file=output)
 output.close()
 
-
-density = 6.506
-atomic_mass = 91.22364157600980
-atoms_per_mole = 0.6022
-
-# fig, ax = plt.subplots(1, 1, figsize=(10,10))
-# # ax.loglog(energy_group[:-1], global_element['Zr'].recoil_pka_spectrum)
-# ax.loglog(energy_group[:-1], global_recoil['H-1'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='g', label='H-1')
-# ax.loglog(energy_group[:-1], global_recoil['He-4'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='m', label='He-4')
-# ax.loglog(energy_group[:-1], global_recoil['Zr-90'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='b', label='Zr-90')
-# ax.loglog(energy_group[:-1], global_recoil['Zr-91'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='k', label='Zr-91')
-# ax.loglog(energy_group[:-1], global_recoil['Zr-92'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='y', label='Zr-92')
-# ax.loglog(energy_group[:-1], global_recoil['Zr-94'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='c', label='Zr-94')
-# ax.loglog(energy_group[:-1], global_recoil['Zr-95'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='b', label='Zr-95')
-# ax.loglog(energy_group[:-1], global_recoil['Zr-96'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='m', label='Zr-96')
-# ax.loglog(energy_group[:-1], global_recoil['Zr-97'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='b', label='Zr-97')
-# ax.loglog(energy_group[:-1], global_recoil['Zr-93'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='g', label='Zr-93')
-# ax.loglog(energy_group[:-1], global_recoil['Sr-88'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='r', label='Sr-88')
-# ax.loglog(energy_group[:-1], global_recoil['Y-91'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='k', label='Y-91')
-
-# ax.set_xlim(1e-6, 30)
-# ax.set_ylim(1e+6, 1e+12)
-# ax.legend()
-# ax.grid()
-# plt.show()
-
-pkafig = SpectrumFig(title='PKA spectrum')
-pkafig.add_plot(energy_group[:-1], global_recoil['H-1'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='g', label='H-1')
-pkafig.add_plot(energy_group[:-1], global_recoil['Zr-92'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='y', label='Zr-92')
-pkafig.add_plot(energy_group[:-1], global_recoil['Zr-93'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='k', label='Zr-93')
-pkafig.add_plot(energy_group[:-1], global_recoil['Zr-95'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='b', label='Zr-95')
-pkafig.add_plot(energy_group[:-1], global_recoil['Zr-97'].recoil_pka_spectrum * atoms_per_mole * density / atomic_mass, color='m', label='Zr-97')
-pkafig.s_draw()
+# 结果绘图
+if inp.plot_figure:
+    density = inp.density
+    atomic_mass = inp.atomic_mass
+    plot_global_element_figure(global_element, energy_group[:-1], [], density, atomic_mass, 'Element.png')
+    plot_global_element_figure(global_recoil, energy_group[:-1], [], density, atomic_mass, 'Nuclide.png')
